577e7001d5051c0c0f235a7cb924fa4118199996
Pedro Igor <pedroicandido1@gmail.com>
2021-06-19 18:19:05 -0300

    feat: implementado selects de tipo e turno de escolas

---
+++ src/domain/selectsOptions/schoolShift.js
@@ -0,0 +1,6 @@
+export default [
+  { value: 1, label: 'Manhã' },
+  { value: 2, label: 'Tarde' },
+  { value: 3, label: 'Noite' },
+  { value: 4, label: 'Integral' },
+]
---
+++ src/domain/selectsOptions/schoolType.js
@@ -0,0 +1,4 @@
+export default [
+  { value: 1, label: "Pública" },
+  { value: 2, label: "Privado" },
+]
