1e9cc170f4c3de246af7f6586aa9d7c6fcc2e9f1
Pedro Igor <pedroicandido1@gmail.com>
2021-06-19 18:19:50 -0300

    feat: Adicionado mais algumas validacoes referente ao dados de educacao

--- src/components/educationalData/index.js
+++ src/components/educationalData/index.js
@@ -1,62 +1,126 @@
-import Checkbox from '../checkbox'
-import Input from '../input'
-import Select from '../select'
-import useStyles from './styles'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import { useWatch } from 'react-hook-form'
+import Checkbox from "../checkbox";
+import Input from "../input";
+import Select from "../select";
+import useStyles from "./styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import { useWatch, useFormState, useFormContext } from "react-hook-form";
+import ErrorMessage from '../errorMessage'
+import schoolShiftOptions from "../../domain/selectsOptions/schoolShift";
+import schoolTypeOptions from "../../domain/selectsOptions/schoolType";
 
-
-const EducationData = ({ control }) => {
-
-  const classes = useStyles()
-  const isStudent = useWatch({ control, name: 'isStudent' })
-
+const EducationData = () => {
+  const classes = useStyles();
+  const { control } = useFormContext();
+  const { errors } = useFormState({ control });
+  const isStudent = useWatch({ control, name: "isStudent" });
 
   return (
     <Paper className={classes.paper} elevation={4}>
       <Grid container>
         <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-          <Typography component="h2" variant="h6">3. DADOS EDUCACIONAIS DO CANDIDATO</Typography>
+          <Typography component="h2" variant="h6">
+            3. DADOS EDUCACIONAIS DO CANDIDATO
+          </Typography>
           <Divider />
         </Grid>
       </Grid>
 
-      <Checkbox name="isStudent" control={control} label="É estudante?" checked={isStudent} />
-      {isStudent &&
+      <Checkbox name="isStudent" label="É estudante?" checked={isStudent} />
+      {isStudent && (
         <Grid container spacing={2}>
           <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Nome da Escola/Instituição de Ensino" variant="outlined" helperText=""  />
+            <Input
+              error={errors.schoolName && true}
+              fullWidth
+              helperText={errors.schoolName?.message}
+              label="Nome da Escola/Instituição de Ensino"
+              name="schoolName"
+              variant="outlined"
+            />
           </Grid>
           <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Select control={control} name="" variant="outlined" options={[{ value: 'PB', label: 'Pública' }, { value: 'PR', label: 'Privado' }]} placeholder="Instituição" />
+            <Select
+              name="schoolType"
+              variant="outlined"
+              options={schoolTypeOptions}
+              placeholder="Instituição"
+            />
+            <ErrorMessage>{errors.schoolType?.message}</ErrorMessage>
           </Grid>
           <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Endereço da Instituição de Ensino" variant="outlined" helperText=""  />
+            <Input
+              error={errors.schoolStreet && true}
+              fullWidth
+              helperText={errors.schoolStreet?.message}
+              label="Endereço da Instituição de Ensino"
+              name="schoolStreet"
+              variant="outlined"
+            />
           </Grid>
           <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Bairro" variant="outlined" helperText=""  />
+            <Input
+              error={errors.schoolDistrict && true}
+              fullWidth
+              helperText={errors.schoolDistrict?.message}
+              label="Bairro"
+              name="schoolDistrict"
+              variant="outlined"
+            />
           </Grid>
           <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Serie Atual  serie/ano" variant="outlined" helperText=""  />
+            <Input
+              name=""
+              fullWidth
+              label="Serie Atual  serie/ano"
+              variant="outlined"
+              helperText=""
+            />
           </Grid>
           <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Turma" variant="outlined" helperText=""  />
+            <Input
+              name="schoolClass"
+              fullWidth
+              label="Turma"
+              variant="outlined"
+              helperText=""
+            />
           </Grid>
           <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
-            <Select control={control} name="" variant="outlined" options={[{ value: 1, label: 'Manhã' }, { value: 2, label: 'Tarde' }, { value: 3, label: 'Noite' }]} placeholder="Turno" />
+            <Select
+              name="schoolShift"
+              variant="outlined"
+              options={schoolShiftOptions}
+              placeholder="Turno"
+            />
           </Grid>
           <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Escolaridade" variant="outlined" helperText=""  />
+            <Input
+              name="schooling"
+              fullWidth
+              label="Escolaridade"
+              variant="outlined"
+              helperText=""
+            />
           </Grid>
           <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-            <Input control={control} name="" fullWidth label="Outros cursos já realizados" variant="outlined" helperText=""  multiline rows={4} />
+            <Input
+              error={errors.outherCourses && true}
+              fullWidth
+              helperText={errors.outherCourses?.message}
+              label="Outros cursos já realizados"
+              multiline
+              name="outherCourses"
+              rows={4}
+              variant="outlined"
+            />
           </Grid>
-        </Grid>}
+        </Grid>
+      )}
     </Paper>
-  )
-}
+  );
+};
 
-export default EducationData
+export default EducationData;
