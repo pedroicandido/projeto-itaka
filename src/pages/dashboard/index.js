cab31bc222bf18817594e62e7d39e6b21952dc1e
Pedro Igor <pedroicandido1@gmail.com>
2021-06-28 20:52:35 -0300

    refactor: removido componente de card

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -108,29 +108,7 @@ const Dashboard = () => {
   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
   return (
-    <Grid container spacing={2} style={{ height: '100vh' }}>
-      <Grid item xl={12} lg={4} >
-        <Card>
-          <Grid container>
-            <Grid item xl={9} lg={9} md={6} sm={6} xs={6}>
-              <Grid contaier direction="column">
-                <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Typography>Número de Alunos Ativos</Typography>
-                </Grid>
-                <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Typography component="h3" variant="h6">358</Typography>
-                </Grid>
-                <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Button fullWidth color="primary">Acessar lista com nomes</Button>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid xl={3} lg={3} md={6} sm={6} xs={6} style={{ border: '1px solid red' }}>
-              image
-            </Grid>
-          </Grid>
-        </Card>
-      </Grid>
+    <Grid container spacing={2}>
 
       <Grid item xl={10} lg={10}>
         <Card className={classes.card}>
