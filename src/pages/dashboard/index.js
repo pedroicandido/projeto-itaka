5f8b30673cb5920e16ceb5f71af4c669bb5f6818
Pedro Igor <pedroicandido1@gmail.com>
2021-12-02 10:46:56 -0300

    fix: formatado pagina com prettier

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,20 +1,44 @@
-import { Button, Grid, Typography, List, Divider, Badge } from '@material-ui/core'
-import { PieChart, Pie, Cell, ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, } from 'recharts';
-import Icon1 from '../../assets/customIcons/icon1.png'
-import Icon2 from '../../assets/customIcons/icon2.png'
-import Icon3 from '../../assets/customIcons/icon3.png'
-import Icon4 from '../../assets/customIcons/icon4.png'
-import Card from '../../components/card'
-import useStyles from './styles'
-import IconButton from '@material-ui/core/IconButton';
-import RotateRightIcon from '@material-ui/icons/RotateRight';
+import { useEffect } from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  List,
+  Divider,
+  Badge,
+} from "@material-ui/core";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import Icon1 from "../../assets/customIcons/icon1.png";
+import Icon2 from "../../assets/customIcons/icon2.png";
+import Icon3 from "../../assets/customIcons/icon3.png";
+import Icon4 from "../../assets/customIcons/icon4.png";
+import Card from "../../components/card";
+import useStyles from "./styles";
+import IconButton from "@material-ui/core/IconButton";
+import RotateRightIcon from "@material-ui/icons/RotateRight";
 import MUIDataTable from "mui-datatables";
-
+import { useDispatch, useSelector } from "react-redux";
+import { setKinship } from "../../redux/actions/kinshipActions";
+import useAxios from "../../utils/hooks/useAxios";
 
 const Dashboard = () => {
-
-  const classes = useStyles()
-
+  const api = useAxios();
+  const dispatch = useDispatch();
+  const kinshipOptions = useSelector((state) => state.kinship.options);
+  const classes = useStyles();
+  console.log(kinshipOptions);
   const columns = [
     {
       name: "name",
@@ -22,7 +46,7 @@ const Dashboard = () => {
       options: {
         filter: true,
         sort: true,
-      }
+      },
     },
     {
       name: "company",
@@ -30,7 +54,7 @@ const Dashboard = () => {
       options: {
         filter: true,
         sort: false,
-      }
+      },
     },
     {
       name: "status",
@@ -39,20 +63,20 @@ const Dashboard = () => {
         filter: true,
         sort: false,
         customBodyRender: (value) => {
-          let color = 'primary'
-          if (value === 'Encerrada') {
-            color = 'error'
-          } else if (value === 'Férias') {
-            color = 'secondary'
+          let color = "primary";
+          if (value === "Encerrada") {
+            color = "error";
+          } else if (value === "Férias") {
+            color = "secondary";
           }
           return (
             <>
-              <Badge color={color} variant="dot" badgeContent="" /> <span style={{ marginLeft: 5 }}>{value}</span>
+              <Badge color={color} variant="dot" badgeContent="" />{" "}
+              <span style={{ marginLeft: 5 }}>{value}</span>
             </>
-          )
-        }
+          );
+        },
       },
-
     },
     {
       name: "state",
@@ -60,70 +84,81 @@ const Dashboard = () => {
       options: {
         filter: true,
         sort: false,
-      }
+      },
     },
   ];
 
   const dataTable = [
     { name: "Ceramica", company: "Pedro", status: "Ativa", state: "NY" },
     { name: "Futebol", company: "Fernanda", status: "Férias", state: "CT" },
-    { name: "Capoeira", company: "Joel do Borel", status: "Férias", state: "FL" },
+    {
+      name: "Capoeira",
+      company: "Joel do Borel",
+      status: "Férias",
+      state: "FL",
+    },
     { name: "Balé", company: "Jonh Wick", status: "Encerrada", state: "TX" },
   ];
 
   const dataBar = [
     {
-      name: 'Natação',
+      name: "Natação",
       uv: 4000,
       pv: 2400,
       amt: 2400,
     },
     {
-      name: 'Voley',
+      name: "Voley",
       uv: 3000,
       pv: 1398,
       amt: 2210,
     },
     {
-      name: 'Futebol',
+      name: "Futebol",
       uv: 2000,
       pv: 9800,
       amt: 2290,
     },
     {
-      name: 'Capoeira',
+      name: "Capoeira",
       uv: 2780,
       pv: 3908,
       amt: 2000,
-    }
+    },
   ];
-
 
   const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
   ];
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  useEffect(() => {
+    dispatch(setKinship(api));
+  }, [dispatch]);
 
   return (
     <Grid container spacing={2}>
-
       <Grid item xl={10} lg={10}>
         <Card className={classes.card}>
           <Grid container>
             <Grid item xl={12} lg={12}>
               <Grid container justify="space-between" alignItems="center">
-                <Typography style={{ marginLeft: 20, fontWeight: 'bold', fontSize: 14 }}>Alunos Novos</Typography>
-                <IconButton >
+                <Typography
+                  style={{ marginLeft: 20, fontWeight: "bold", fontSize: 14 }}
+                >
+                  Alunos Novos
+                </Typography>
+                <IconButton>
                   <RotateRightIcon />
                 </IconButton>
               </Grid>
             </Grid>
 
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-              <div style={{ width: '100%', height: 450 }}>
+              <div style={{ width: "100%", height: 450 }}>
                 <ResponsiveContainer>
                   <BarChart
                     data={dataBar}
@@ -135,12 +170,20 @@ const Dashboard = () => {
                     }}
                     barSize={20}
                   >
-                    <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                    <XAxis
+                      dataKey="name"
+                      scale="point"
+                      padding={{ left: 10, right: 10 }}
+                    />
                     <YAxis />
                     <Tooltip />
                     <Legend />
                     <CartesianGrid strokeDasharray="3 3" />
-                    <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+                    <Bar
+                      dataKey="pv"
+                      fill="#8884d8"
+                      background={{ fill: "#eee" }}
+                    />
                   </BarChart>
                 </ResponsiveContainer>
               </div>
@@ -155,8 +198,12 @@ const Dashboard = () => {
           <Grid container>
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
               <Grid container justify="space-between" alignItems="center">
-                <Typography style={{ marginLeft: 20, fontWeight: 'bold', fontSize: 14 }}>Média de idade dos alunos</Typography>
-                <IconButton >
+                <Typography
+                  style={{ marginLeft: 20, fontWeight: "bold", fontSize: 14 }}
+                >
+                  Média de idade dos alunos
+                </Typography>
+                <IconButton>
                   <RotateRightIcon />
                 </IconButton>
               </Grid>
@@ -164,9 +211,9 @@ const Dashboard = () => {
             </Grid>
 
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-              <div style={{ width: '100%', height: 300 }}>
+              <div style={{ width: "100%", height: 300 }}>
                 <ResponsiveContainer>
-                  <PieChart onMouseEnter={() => { }}>
+                  <PieChart onMouseEnter={() => {}}>
                     <Pie
                       data={data}
                       innerRadius={60}
@@ -176,7 +223,10 @@ const Dashboard = () => {
                       dataKey="value"
                     >
                       {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={COLORS[index % COLORS.length]}
+                        />
                       ))}
                     </Pie>
                     <Legend />
@@ -194,8 +244,12 @@ const Dashboard = () => {
           <Grid container>
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
               <Grid container justify="space-between" alignItems="center">
-                <Typography style={{ marginLeft: 20, fontWeight: 'bold', fontSize: 14 }}>Relatórios</Typography>
-                <IconButton >
+                <Typography
+                  style={{ marginLeft: 20, fontWeight: "bold", fontSize: 14 }}
+                >
+                  Relatórios
+                </Typography>
+                <IconButton>
                   <RotateRightIcon />
                 </IconButton>
               </Grid>
@@ -205,47 +259,64 @@ const Dashboard = () => {
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
               <Grid container spacing={1} alignItems="center">
                 <Grid item xl={4} lg={4}>
-                  <img src={Icon1} style={{ width: '100%', height: '100%' }} />
+                  <img src={Icon1} style={{ width: "100%", height: "100%" }} />
                 </Grid>
                 <Grid item xl={8} lg={8}>
-                  <Typography style={{ fontSize: 13, fontWeight: 'bold' }}>Média de Idades</Typography>
-                  <Typography style={{ cursor: 'pointer', fontSize: 12 }}>Exportar Lista</Typography>
+                  <Typography style={{ fontSize: 13, fontWeight: "bold" }}>
+                    Média de Idades
+                  </Typography>
+                  <Typography style={{ cursor: "pointer", fontSize: 12 }}>
+                    Exportar Lista
+                  </Typography>
                 </Grid>
-
               </Grid>
               <Divider />
               <Grid container spacing={1} alignItems="center">
                 <Grid item xl={4} lg={4}>
-                  <img src={Icon2} style={{ width: '100%', height: '100%' }} />
+                  <img src={Icon2} style={{ width: "100%", height: "100%" }} />
                 </Grid>
                 <Grid item xl={8} lg={8}>
-                  <Typography style={{ fontSize: 13, fontWeight: 'bold' }}>Média SocioEco</Typography>
-                  <Typography style={{ cursor: 'pointer', fontSize: 12 }}>Exportar Lista</Typography>
+                  <Typography style={{ fontSize: 13, fontWeight: "bold" }}>
+                    Média SocioEco
+                  </Typography>
+                  <Typography style={{ cursor: "pointer", fontSize: 12 }}>
+                    Exportar Lista
+                  </Typography>
                 </Grid>
-              </Grid>
-
-              <Divider />
-              <Grid container spacing={1} alignItems="center">
-                <Grid item xl={4} lg={4}>
-                  <img src={Icon3} style={{ width: '100%', height: '100%' }} />
-                </Grid>
-                <Grid item xl={8} lg={8}>
-                  <Typography style={{ fontSize: 13, fontWeight: 'bold', textAlign: 'center' }}>{`Relação Menino x Menina`}</Typography>
-                  <Typography style={{ cursor: 'pointer', fontSize: 12 }}>Exportar Lista</Typography>
-                </Grid>
-
               </Grid>
 
               <Divider />
               <Grid container spacing={1} alignItems="center">
                 <Grid item xl={4} lg={4}>
-                  <img src={Icon4} style={{ width: '100%', height: '100%' }} />
+                  <img src={Icon3} style={{ width: "100%", height: "100%" }} />
                 </Grid>
                 <Grid item xl={8} lg={8}>
-                  <Typography style={{ fontSize: 13, fontWeight: 'bold' }}>Evasão</Typography>
-                  <Typography style={{ cursor: 'pointer', fontSize: 12 }}>Exportar Lista</Typography>
+                  <Typography
+                    style={{
+                      fontSize: 13,
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >{`Relação Menino x Menina`}</Typography>
+                  <Typography style={{ cursor: "pointer", fontSize: 12 }}>
+                    Exportar Lista
+                  </Typography>
                 </Grid>
+              </Grid>
 
+              <Divider />
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xl={4} lg={4}>
+                  <img src={Icon4} style={{ width: "100%", height: "100%" }} />
+                </Grid>
+                <Grid item xl={8} lg={8}>
+                  <Typography style={{ fontSize: 13, fontWeight: "bold" }}>
+                    Evasão
+                  </Typography>
+                  <Typography style={{ cursor: "pointer", fontSize: 12 }}>
+                    Exportar Lista
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
@@ -253,15 +324,10 @@ const Dashboard = () => {
       </Grid>
 
       <Grid item xl={10} lg={10} md={12} sm={12} xs={12}>
-        <MUIDataTable
-          title="Oficinas"
-          data={dataTable}
-          columns={columns}
-        />
+        <MUIDataTable title="Oficinas" data={dataTable} columns={columns} />
       </Grid>
-
     </Grid>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
