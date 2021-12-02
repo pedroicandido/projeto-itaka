c1b57154879438a47e4e6e05bc90af09adece023
Pedro Igor <pedroicandido1@gmail.com>
2021-12-02 15:43:53 -0300

    feat: add borda em select indicando erro

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,23 +1,33 @@
-import Select from 'react-select'
-import { useController, useFormContext } from "react-hook-form";
+import Select from "react-select";
+import { useController, useFormContext, useFormState } from "react-hook-form";
 
 const CustomSelectInput = ({ control, name, ...rest }) => {
-  const { control: controlContext } = useFormContext()
+  const { control: controlContext } = useFormContext();
+  const { errors } = useFormState({ control: control ?? controlContext });
   const { field } = useController({
     name,
-    control: control ?? controlContext
+    control: control ?? controlContext,
   });
 
-  return <Select {...field} {...rest} styles={{
-    control: (provided) => ({
-      ...provided,
-      height: 55,
-    }),
-    menu: (provided, state) => ({
-      ...provided,
-      zIndex: 1000,
-    }),
-  }} />;
-}
+  const hasError = Boolean(errors[name]);
 
-export default CustomSelectInput
+  return (
+    <Select
+      {...field}
+      {...rest}
+      styles={{
+        control: (provided) => ({
+          ...provided,
+          height: 55,
+          borderColor: hasError ? "red": 'hsl(0, 0%, 80%)',
+        }),
+        menu: (provided, state) => ({
+          ...provided,
+          zIndex: 1000,
+        }),
+      }}
+    />
+  );
+};
+
+export default CustomSelectInput;
