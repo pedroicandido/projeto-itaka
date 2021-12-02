cc32df5f4dac0f2406b6ccc0f71a44502baa295d
Pedro Igor <pedroicandido1@gmail.com>
2021-12-02 10:47:43 -0300

    refactor: alterado app para seu funcionamento normal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Layout from './hoc/layout'
-import RegisterCandidate from './pages/candidate/addCandidate'
-import Routes from './routes'
-import SignIn from './pages/signin'
-import CssBaseline from '@material-ui/core/CssBaseline';
-
+import Routes from "./routes";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
-
-
-
   return (
     <>
       <CssBaseline />
