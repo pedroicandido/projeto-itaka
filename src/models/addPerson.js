902d53ffda0fbe090ed108a623340721f70ccd44
Pedro Igor <pedroicandido1@gmail.com>
2021-12-04 14:20:46 -0300

    feat: add model de addPerson

--- src/models/addPerson.js
+++ src/models/addPerson.js
@@ -8,7 +8,10 @@ export default class AddPersonModel {
     rg,
     skinColor,
     address,
-    houseNumber
+    houseNumber,
+    workSituation,
+    emissary,
+    income
   }) {
     this.estado_civil = civilStatus;
     this.dt_nascimento = birthDate;
@@ -16,8 +19,11 @@ export default class AddPersonModel {
     this.doc = document;
     this.nome = name;
     this.rg = rg;
+    this.rg_orgao_expedidor = emissary;
     this.etnia_id = skinColor;
     this.logradouro_id = address;
     this.numero_lograd = houseNumber
+    this.sit_trabalhista_id = workSituation;
+    this.renda = income
   }
 }
